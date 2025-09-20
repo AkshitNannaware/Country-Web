5daedbef5736227b084606e9c000691376168aa0
Akshit <akshitnannaware@gmail.com>
2025-09-20 21:25:20 +0530

    Initial commit

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 const Header = () => {    
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+    const closeMobileMenu = () => {
+        setIsMobileMenuOpen(false);
+    };
+
     return (
         <header>
             <div className='container'>
                  <div className='grid navbar-grid'>
                     <div className='Logo'>
-                        <NavLink to = "/">
+                        <NavLink to = "/" onClick={closeMobileMenu}>
                         <h1>WorldAtlas</h1>
                         </NavLink>
                     </div>
 
-                    <nav>
+                    <nav className='menu-web'>
                         <ul>
                             <li>
                                 <NavLink to = "/">Home</NavLink>
@@ -28,7 +39,33 @@ const Header = () => {
                             </li>
                         </ul>
                     </nav>
+
+                    <div className='ham-menu'>
+                        <button onClick={toggleMobileMenu} className='mobile-menu-btn'>
+                            {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+                        </button>
+                    </div>
                  </div>
+
+                 {/* Mobile Menu */}
+                 {isMobileMenuOpen && (
+                     <nav className='menu-mobile'>
+                         <ul>
+                             <li>
+                                 <NavLink to = "/" onClick={closeMobileMenu}>Home</NavLink>
+                             </li>
+                             <li>
+                                 <NavLink to = "/about" onClick={closeMobileMenu}>About</NavLink>
+                             </li>
+                             <li>
+                                 <NavLink to = "/contact" onClick={closeMobileMenu}>Contact</NavLink>
+                             </li>
+                             <li>
+                                 <NavLink to = "/country" onClick={closeMobileMenu}>Country</NavLink>
+                             </li>
+                         </ul>
+                     </nav>
+                 )}
             </div>
         </header>
     )
